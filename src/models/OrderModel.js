ae7f050940975c1e9ab49585d545a88752be1d41
atikurahad <atikurahad@gmail.com>
2023-12-09 02:08:59 +0600

    models are ready

---
+++ src/models/OrderModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const DataSchema = mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, required: true },
+
+    items: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+
+    totalAmount: { type: Number, required: true, min: 1 },
+
+    shippingAddress: { type: String, required: true },
+
+    status: { type: String, required: true, default: "Pending" },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const OrderModel = mongoose.model("items", DataSchema);
+
+module.exports = OrderModel;
